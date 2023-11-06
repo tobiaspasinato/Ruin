6e46e6966bec92299b937cf3b27f92dadc991536
tobiaspasinato <tobiasmarpasinato2005@gmail.com>
2023-11-06 14:32:41 -0300

    12

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ pygame.init() #Se inicializa pygame
 pygame.display.set_caption("Ruin") # Nombre de la pestaña
 screen = pygame.display.set_mode([LARGO_PANTALLA, ANCHO_PANTALLA]) #Se crea una ventana
 running = True
+
 while running:
 # Se verifica si el usuario cerro la ventana
     for event in pygame.event.get():
@@ -30,9 +31,9 @@ while running:
                 print("d")
             if lista_teclas[K_l]:
                 print("l")
-
-screen.fill((255, 255, 255))# Se pinta el fondo de la ventana
-# Se dibuja un círculo azul en el centro
-pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
-pygame.display.flip()# Muestra los cambios en la pantalla
+    
+    pygame.display.flip() # Muestra los cambios en la pantalla
+    screen.fill((0, 0, 255))# Se pinta el fondo de la ventana
+    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)# Se dibuja un círculo azul en el centro
+    pygame.display.flip()# Muestra los cambios en la pantalla
 pygame.quit() # Fin
