03a46bd91e9e42772584e0593e8900809ad0dbf2
tobiaspasinato <tobiasmarpasinato2005@gmail.com>
2023-11-08 15:31:45 -0300

    142342

--- clases/player.py
+++ clases/player.py
@@ -1,4 +1,15 @@
+import pygame
+
+def get_animacion():
+    lista = []
+    return lista
+
 class player:
     def __init__(self) -> None:
+        self.walk = []
+        self.stay = []
         self.frame = 0
+        self.animation = self.stay
+        self.image = self.animation[self.frame]
+        self.react = self.image.get_react()
         pass
