ad0c7b674f1dd7842e51efbce6c669a46296c329
tobiaspasinato <tobiasmarpasinato2005@gmail.com>
2023-11-23 19:41:53 -0300

    1234124

--- main.py
+++ main.py
@@ -8,18 +8,32 @@ from constants import *
 
 direccion = True
 score = 0
+segundos = 0
 level1 = True
 level2 = False
 level3 = False
+player_level1 = True
+player_level2 = False
+player_level3 = False
+flag_coin1 = False
+flag_coin2 = False
+flag_coin3 = False
+fin_timer = False
+win_flag = False
+
 milis = pygame.time.Clock()
 
 pygame.init() #Se inicializa pygame
+timer_segundos = pygame.USEREVENT
+pygame.time.set_timer(timer_segundos, 1000)
 pygame.mixer.init()
 pygame.mixer.music.set_volume(0.7)
 sonido_fondo = pygame.mixer.Sound("music\music_game.wav")
 sonido_fondo.set_volume(VOLUMEN_MUSICA)
 font = pygame.font.SysFont("Arial Narrow", 50)
 text_score = font.render(f"Score: {score}", True, (0, 0, 0))
+text_time = font.render(f"Time: {segundos}", True, (0, 0, 0))
+text_restart = font.render("Restart", True, (255, 255, 255))
 pygame.display.set_caption("Ruin") # Nombre de la pestaña
 imagen_level1 = pygame.image.load("./imgs/level 1.png") # Cargar imagen del fondo
 level1_img = pygame.transform.scale(imagen_level1, (LARGO_PANTALLA, ANCHO_PANTALLA))
@@ -27,29 +41,19 @@ imagen_level2 = pygame.image.load("./imgs/level 2.png") # Cargar imagen del fond
 level2_img = pygame.transform.scale(imagen_level2, (LARGO_PANTALLA, ANCHO_PANTALLA))
 imagen_level3 = pygame.image.load("./imgs/level 3.png") # Cargar imagen del fondo
 level3_img = pygame.transform.scale(imagen_level3, (LARGO_PANTALLA, ANCHO_PANTALLA))
+imagen_ganar = pygame.image.load("imgs\end_img.png") # Cargar imagen del fondo
+win_img = pygame.transform.scale(imagen_ganar, (LARGO_PANTALLA, ANCHO_PANTALLA))
 screen = pygame.display.set_mode([LARGO_PANTALLA, ANCHO_PANTALLA]) #Se crea una ventana
 
-if level1 == True:
-    flag_coin1 = False
-    player1 = player(175, 0)
-    # enemy1 = enemy(250, 250)
-    coin1 = coin(200, 200)
-    bandera1 = bandera(150, 300)
-elif level2 == True:
-    flag_coin2 = False
-    player2 = player(175, 0)
-    # enemy1 = enemy(250, 250)
-    coin2 = coin(200, 200)
-    bandera2 = bandera(150, 300)
-elif level3 == True:
-    flag_coin3 = False
-    player3 = player(175, 0)
-    # enemy1 = enemy(250, 250)
-    coin3 = coin(200, 200)
-    bandera3 = bandera(150, 300)
+player1 = player(175, 0)
+coin1 = coin(335, 298)
+bandera1 = bandera(55, 470)
+coin2 = coin(125, 340)
+bandera2 = bandera(255, 5)
+coin3 = coin(100, 175)
+bandera3 = bandera(250, 220)
 
 running = True
-
 
 while running:
     accion_personaje = "stay"
@@ -57,52 +61,130 @@ while running:
     for event in pygame.event.get():# Se verifica si el usuario cerro la ventana
         if event.type == pygame.QUIT:
             running = False
-    lista_teclas = pygame.key.get_pressed()
-    if True in lista_teclas:
-        if lista_teclas[K_a]:
-            direccion = False
-            accion_personaje = "walk"
-            player1.control(-2, 0)
-        if lista_teclas[K_s]:
-            accion_personaje = "walk"
-            player1.control(0, 2)
-        if lista_teclas[K_w]:
-            accion_personaje = "walk"
-            player1.control(0, -2)
-        if lista_teclas[K_d]:
-            direccion = True
-            accion_personaje = "walk"
-            player1.control(2, 0)
-        if lista_teclas[K_m]:
-            sonido_fondo.stop()
-    
-    text_score = font.render(f"Score: {score}", True, (0, 0, 0))
-    screen.fill((0, 0, 0))# Se pinta el fondo de la ventana
-    if level1 == True:
-        screen.blit(level1_img,(0,0)) # Ubicacion del fondo
-    elif level2 == True:
-        screen.blit(level2_img,(0,0)) # Ubicacion del fondo
-    elif level3 == True:
-                screen.blit(level3_img,(0,0)) # Ubicacion del fondo
-    screen.blit(text_score,(350,10))
-    # enemy1.moves()
-    # enemy1.upgrade()
-    # enemy1.dibujar(screen)
-    if flag_coin1 == False:
-        coin1.upgrade()
-        coin1.dibujar(screen)
-        if player1.rect.colliderect(coin1.rect):
-            flag_coin1 = True
-            score = score + 10
-    if flag_coin1 == True:
-        bandera1.upgrade()
-        bandera1.dibujar(screen)
-        if player1.rect.colliderect(bandera1.rect):
-            level1 = False
-            level2 = True
-            level3 = False
-    player1.upgrade()
-    player1.dibujar(screen, accion_personaje, direccion)
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse = event.pos
+            print(mouse)
+            if win_flag == True:
+                if (mouse[0] > 0 and mouse[0] < 140) and (mouse[1] < 500 and mouse[1] > 440):
+                    direccion = True
+                    score = 0
+                    segundos = 0
+                    level1 = True
+                    level2 = False
+                    level3 = False
+                    player_level1 = True
+                    player_level2 = False
+                    player_level3 = False
+                    flag_coin1 = False
+                    flag_coin2 = False
+                    flag_coin3 = False
+                    fin_timer = False
+                    win_flag = False
+        if event.type == timer_segundos:
+            if fin_timer == False:
+                segundos = segundos + 1
+    if win_flag == False:
+        lista_teclas = pygame.key.get_pressed()
+        if True in lista_teclas:
+            if lista_teclas[K_a]:
+                direccion = False
+                accion_personaje = "walk"
+                player1.control(-3, 0)
+            if lista_teclas[K_s]:
+                accion_personaje = "walk"
+                player1.control(0, 3)
+            if lista_teclas[K_w]:
+                accion_personaje = "walk"
+                player1.control(0, -3)
+            if lista_teclas[K_d]:
+                direccion = True
+                accion_personaje = "walk"
+                player1.control(3, 0)
+            if lista_teclas[K_m]:
+                sonido_fondo.stop()
+        
+        text_score = font.render(f"Score: {score}", True, (0, 0, 0))
+        text_time = font.render(f"Time: {segundos}", True, (0, 0, 0))
+        screen.fill((0, 0, 0))# Se pinta el fondo de la ventana
+        if level1 == True:
+            screen.blit(level1_img,(0,0)) # Ubicacion del fondo
+        elif level2 == True:
+            screen.blit(level2_img,(0,0)) # Ubicacion del fondo
+        elif level3 == True:
+            screen.blit(level3_img,(0,0)) # Ubicacion del fondo
+        screen.blit(text_score,(325,10))
+        screen.blit(text_time,(10,10))
+        if level1 == True:
+            if player_level1 == True:
+                player1.move_x = 175
+                player1.move_y = 0
+                player_level1 = False
+                player_level2 = True
+                player_level3 = False
+            if flag_coin1 == False:
+                coin1.upgrade()
+                coin1.dibujar(screen)
+                if player1.rect.colliderect(coin1.rect):
+                    flag_coin1 = True
+                    score = score + 10
+            if flag_coin1 == True:
+                bandera1.upgrade()
+                bandera1.dibujar(screen)
+                if player1.rect.colliderect(bandera1.rect):
+                    level1 = False
+                    level2 = True
+                    level3 = False
+        if level2 == True:
+            if player_level2 == True:
+                player1.move_x = 450
+                player1.move_y = 300
+                player_level1 = False
+                player_level2 = False
+                player_level3 = True
+            if flag_coin2 == False:
+                coin2.upgrade()
+                coin2.dibujar(screen)
+                if player1.rect.colliderect(coin2.rect):
+                    flag_coin2 = True
+                    score = score + 10
+            if flag_coin2 == True:
+                bandera2.upgrade()
+                bandera2.dibujar(screen)
+                if player1.rect.colliderect(bandera2.rect):
+                    level1 = False
+                    level2 = False
+                    level3 = True
+        if level3 == True:
+            if player_level3 == True:
+                player1.move_x = 250
+                player1.move_y = 450
+                player_level1 = False
+                player_level2 = False
+                player_level3 = False
+            if flag_coin3 == False:
+                coin3.upgrade()
+                coin3.dibujar(screen)
+                if player1.rect.colliderect(coin3.rect):
+                    flag_coin3 = True
+                    score = score + 10
+            if flag_coin3 == True:
+                bandera3.upgrade()
+                bandera3.dibujar(screen)
+                if player1.rect.colliderect(bandera3.rect):
+                    level1 = False
+                    level2 = False
+                    level3 = False
+                    fin_timer = True
+                    win_flag = True
+        player1.upgrade()
+        player1.dibujar(screen, accion_personaje, direccion)
+    if win_flag == True:
+        screen.blit(win_img,(0,0)) # Ubicacion del fondo
+        screen.blit(text_restart,(10,450))
     pygame.display.flip()# Muestra los cambios en la pantalla
     milis.tick(FPS)
 pygame.quit() # Fin
+
+"""
+nota, me quede en la parte de ubicar al personaje una vez pasa de nivel
+"""
